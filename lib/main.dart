e4bb49b97abc49c896d93de1fca4223ea21e1021
Saran M S <saran.1801208@srec.ac.in>
2020-05-21 18:12:14 +0530

    full functionality added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,30 +16,50 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Row(
+  _DicePageState createState() => _DicePageState();
+}
+  int leftDiceNumber=1;
+  int rightDiceNumber=1;
 
-        children: <Widget>[
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Image(
-                image: AssetImage('images/dice1.png'),
+  class _DicePageState extends State<DicePage> {
+    @override
+    Widget build(BuildContext context) {
+      return Center(
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              child: FlatButton(
+                onPressed: () {
+                  setState(() {
+                    getRandomDiceNumber();
+                  });
+                },
+                child: Image(
+                  image: AssetImage('images/dice$leftDiceNumber.png'),
+                ),
               ),
             ),
-          ),
-
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Image.asset('images/dice2.png'),
+            Expanded(
+              child: FlatButton(
+                onPressed: () {
+                  setState(() {
+                   getRandomDiceNumber();
+                  });
+                },
+                child: Image.asset('images/dice$rightDiceNumber.png'),
+              ),
             ),
-          ),
-        ],
-      ),
-    );
+          ],
+        ),
+      );
+    }
+    void getRandomDiceNumber() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    }
   }
-}
+
+
+
